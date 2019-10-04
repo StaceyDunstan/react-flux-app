fbde30e65f0be47ba309f857c03e98c020319a64
stacey dunstan <stacey.dunstan40@gmail.com>
2019-10-04 20:34:26 +0100

    routing

---
+++ src/components/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <>
+              <h2>About</h2>
+              <p>This app is the Cawsand about page.</p>
+            </>
+        );
+    }
+}
+export default AboutPage;
